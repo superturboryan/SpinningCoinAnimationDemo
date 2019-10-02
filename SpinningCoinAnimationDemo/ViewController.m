5fdc207357538cb54130ed9396c3982e4df3fb81
Ryan David Forsyth <forsyth.r@gmail.com>
2019-10-01 23:54:45 -0400

    More animations

--- SpinningCoinAnimationDemo/ViewController.m
+++ SpinningCoinAnimationDemo/ViewController.m
@@ -39,10 +39,12 @@
     
     [gradient setColors: @[(id)[UIColor colorWithRed:15.0f/255.0f green:12.0f/255.0f blue:41.0f/255.0f alpha:1].CGColor,(id)[UIColor colorWithRed:48.0f/255.0f green:43.0f/255.0f blue:99.0f/255.0f alpha:1].CGColor,(id)[UIColor colorWithRed:36.0f/255.0f green:36.0f/255.0f blue:62.0f/255.0f alpha:1].CGColor]];
     
-    
     [self.contentView.layer insertSublayer:gradient atIndex:0];
     
+    self.spinButton.layer.cornerRadius = 5.0f;
 }
+
+
 
 - (IBAction)pressSpin:(id)sender {
     [self hideNumber];
@@ -51,20 +53,22 @@
 
 -(void)performSpinAnimation
 {
+    float randomSpinCount = roundf((((float)rand() / RAND_MAX) * 4) + 3);
+    float randomDuration = (((float)rand() / RAND_MAX) * 0.15) + 0.35;
+    
     CATransition* transition = [CATransition animation];
     transition.delegate = self;
     transition.startProgress = 0;
     transition.endProgress = 1.0;
     transition.type = @"flip";
     transition.subtype = @"fromRight";
-    transition.duration = 0.45;
+    transition.duration = randomDuration;
     transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
-    float randomNumThreeToSeven = roundf((((float)rand() / RAND_MAX) * 4) + 3);
-    [self setNumToDisplay:[NSNumber numberWithFloat:randomNumThreeToSeven]];
-    transition.repeatCount = randomNumThreeToSeven;
+    transition.repeatCount = randomSpinCount;
+
+    [self setNumToDisplay:[NSNumber numberWithFloat:randomSpinCount]];
 
     [self.coinImage.layer addAnimation:transition forKey:@"transition"];
-    
 }
 
 -(void)displayNumber:(NSNumber*)number
@@ -74,6 +78,17 @@
         [self.numberLabel setAlpha: 1];
         [self.numberLabel setText:label];
     } completion:nil];
+    
+    [UIView animateWithDuration:0.35 delay:0.5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        CGAffineTransform bigger = CGAffineTransformMakeScale(1.05, 1.05);
+        self.coinImage.transform = bigger;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.25 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            CGAffineTransform normal = CGAffineTransformMakeScale(1, 1);
+            self.coinImage.transform = normal;
+        } completion:nil];
+        
+    }];
 }
 
 -(void)hideNumber
